8aed0d87bef5cf298d3dcc3e20632fdef3d6a4f0
Andrew Thornton <art27@cantab.net>
2022-12-29 19:00:50 +0000

    Add convenience functions and do some more refactoring
    Signed-off-by: Andrew Thornton <art27@cantab.net>

--- modules/packages/npm/creator.go
+++ modules/packages/npm/creator.go
@@ -22,11 +22,16 @@ import (
 )
 
 var (
-	ErrInvalidPackage        = util.SilentWrap{Message: "package is invalid", Err: util.ErrInvalidArgument}
-	ErrInvalidPackageName    = util.SilentWrap{Message: "package name is invalid", Err: util.ErrInvalidArgument}
-	ErrInvalidPackageVersion = util.SilentWrap{Message: "package version is invalid", Err: util.ErrInvalidArgument}
-	ErrInvalidAttachment     = util.SilentWrap{Message: "package attachment is invalid", Err: util.ErrInvalidArgument}
-	ErrInvalidIntegrity      = util.SilentWrap{Message: "failed to validate integrity", Err: util.ErrInvalidArgument}
+	// ErrInvalidPackage indicates an invalid package
+	ErrInvalidPackage = util.NewInvalidArgumentErrorf("The package is invalid")
+	// ErrInvalidPackageName indicates an invalid name
+	ErrInvalidPackageName = util.NewInvalidArgumentErrorf("The package name is invalid")
+	// ErrInvalidPackageVersion indicates an invalid version
+	ErrInvalidPackageVersion = util.NewInvalidArgumentErrorf("The package version is invalid")
+	// ErrInvalidAttachment indicates a invalid attachment
+	ErrInvalidAttachment = util.NewInvalidArgumentErrorf("The package attachment is invalid")
+	// ErrInvalidIntegrity indicates an integrity validation error
+	ErrInvalidIntegrity = util.NewInvalidArgumentErrorf("Failed to validate integrity")
 )
 
 var nameMatch = regexp.MustCompile(`\A((@[^\s\/~'!\(\)\*]+?)[\/])?([^_.][^\s\/~'!\(\)\*]+)\z`)
--- modules/packages/nuget/symbol_extractor.go
+++ modules/packages/nuget/symbol_extractor.go
@@ -18,10 +18,10 @@ import (
 )
 
 var (
-	ErrMissingPdbFiles       = util.SilentWrap{Message: "package does not contain PDB files", Err: util.ErrInvalidArgument}
-	ErrInvalidFiles          = util.SilentWrap{Message: "package contains invalid files", Err: util.ErrInvalidArgument}
-	ErrInvalidPdbMagicNumber = util.SilentWrap{Message: "invalid Portable PDB magic number", Err: util.ErrInvalidArgument}
-	ErrMissingPdbStream      = util.SilentWrap{Message: "missing PDB stream", Err: util.ErrInvalidArgument}
+	ErrMissingPdbFiles       = util.NewInvalidArgumentErrorf("Package does not contain PDB files")
+	ErrInvalidFiles          = util.NewInvalidArgumentErrorf("Package contains invalid files")
+	ErrInvalidPdbMagicNumber = util.NewInvalidArgumentErrorf("Invalid Portable PDB magic number")
+	ErrMissingPdbStream      = util.NewInvalidArgumentErrorf("Missing PDB stream")
 )
 
 type PortablePdb struct {
